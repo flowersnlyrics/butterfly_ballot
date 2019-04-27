232ab2abe963670d38a90645838997024304804e
Jennifer de Souza <jenniferdesouza88@gmail.com>
2019-04-27 19:25:22 -0400

    mocha test makes sure we have the ability to deploy an instantiation of Ballot.sol contract

--- blockchain/contracts/Ballot.sol
+++ blockchain/contracts/Ballot.sol
@@ -1,14 +1,18 @@
 pragma solidity ^0.4.17; 
 
 contract Ballot {
-    address public manager; // using public or private enforces no security
+    address public voteKeeper; // using public or private enforces no security
     address[] public voters; 
     
     function Ballot() public {
-        manager = msg.sender; 
+        voteKeeper = msg.sender; 
     }
-
-    function enter() public payable {
+    
+    // 
+    // Here we push the user's vote 
+    // and also check that this voter hasn't voted before 
+    //
+    function submitVote() public payable {
         require(!hasAlreadyVoted(msg.sender)); 
         // put in voters array 
         voters.push(msg.sender); 
@@ -24,28 +28,24 @@ contract Ballot {
         return false; 
     }
     
-    function random() private view returns (uint){
-        return uint(keccak256(block.difficulty, now, voters)); // hash is hex #
-    }
-    
     // public or private not used to enforce security
     // require used to enforce security 
     function pickWinner() public restricted{
         //require(msg.sender == manager); // no one can call this function except the person who created the contract 
         
-        uint index = random() % voters.length; 
-        voters[index].transfer(this.balance); // need to send ether represented in contract 
+        //uint index = random() % voters.length; 
+        //voters[index].transfer(this.balance); // need to send ether represented in contract 
         // might try to reset contract after picking a winner, infinite series of lotteries
         // empty out list of addresses
         voters = new address[](0); // new brand new dymanic array, (0) initial size of 0
     }
     
     modifier restricted(){
-        require(msg.sender == manager);
+        require(msg.sender == voteKeeper);
         _; 
     }
     
-    function getPlayers() public view returns (address[]) {
+    function getVoters() public view returns (address[]) {
         return voters; 
     }
 }
--- blockchain/test/Ballot.test.js
+++ blockchain/test/Ballot.test.js
@@ -22,15 +22,29 @@ beforeEach(async () => {
 
 
 describe('Ballot Contract', () => {
-    it('deploys a contract', () => {
+    
+    // What behavior do you really care about with this contract?
+    
+    //
+    // TEST #1
+    // Make sure this schtuff actually deploys a contract
+    //
+    it('Ballot.sol instantiation is deployed correctly', () => {
         assert.ok(ballot.options.address);
     });
 
-    // What behavior do you really care about with this contract?
+    //
+    // TEST #2 
+    // Make sure that the person who deploys the contract
+    // is the voteKeeper
+    //
 
-    // Any time someone calls enter, make sure their address gets
-    // added if they send the correct amount of money
-    it('allows one account to enter', async() => {
+    //
+    // TEST #3
+    // Any time someone enters their vote, make sure 
+    // they're added to the voter array 
+    //
+    /*it('When account votes, should be added to voters array', async() => {
         await ballot.methods.enter().send({
             from: accounts[0], 
             value: web3.utils.toWei('0.02', 'ether') 
@@ -42,100 +56,108 @@ describe('Ballot Contract', () => {
         
         assert.equal(accounts[0], players[0]); // should be, is 
         assert.equal(1, players.length); 
-    });
+    });*/
+    
+    //
+    // TEST #4
+    // Make sure the same account doesn't vote twice 
+    //
+
+    // Any time someone calls enter, make sure their address gets
+    // added if they send the correct amount of money
 
     // able to enter on multiple accounts and that they 
     // are stored in the players array 
-    it('allows mutiple account to enter', async() => {
-        
-        // 1 account enters
-        await ballot.methods.enter().send({
-            from: accounts[0], 
-            value: web3.utils.toWei('0.02', 'ether') 
-        }); 
-        // 2 accounts enter 
-        await ballot.methods.enter().send({
-            from: accounts[1], 
-            value: web3.utils.toWei('0.02', 'ether') 
-        }); 
-        // 3 accounts enter 
-        await ballot.methods.enter().send({
-            from: accounts[2], 
-            value: web3.utils.toWei('0.02', 'ether') 
-        }); 
-
-
-        const players = await ballot.methods.getPlayers().call({
-            from: accounts[0]
-        });
-        
-        assert.equal(accounts[0], players[0]);  // is, should be 
-        assert.equal(accounts[1], players[1]);  // is, should be 
-        assert.equal(accounts[2], players[2]);  // is, should be 
-        assert.equal(3, players.length); 
-    });
-
-    // make sure person can enter if they send less than more ether
-    it('requires a minimum amt. of ether to enter', async() => {
-        try{
-            await ballot.methods.enter().send({
-                from: accounts[0], 
-                value: 200 // Wei
-            });
-            assert(false); // test fails here  
-        } catch(err){
-           assert(err); // check for truthfulness, we should get here cause no ether :( 
-           
-        }
-
-    });
-    
-    // if some person other than the manager attemps to 
-    // pick a winner an error should be thrown
-    it('only manager can call pickWinner', async() => {
-        try{
-            await ballot.methods.pickWinner().send({
-                from: accounts[1]
-            });
-
-            assert(false); //automatically fails the test no matter what
-        } catch(err) {
-            assert(err); // error should be thrown since ACCT.1 is not the manager (acct 0 is)  
-        }
-    });
-
-    // end to end test
-    // winner gets money, players array gets reset
-    it('sends money to the winner and resets the players array', async() => {
-        await ballot.methods.enter().send({
-            from: accounts[0],
-            value: web3.utils.toWei('2', 'ether')
-        });
-
-        // only enter accounts[0] into ballot so that it def wins
-        // see if accounts[0] balance before and after is different
-        // should be more after calling pickWinner
-        const initialBalance = await web3.eth.getBalance(accounts[0]); // units of wei
-        
-        // spent gas here 
-        await ballot.methods.pickWinner().send({from: accounts[0]});
-        const finalBalance = await web3.eth.getBalance(accounts[0]);
-
-        // wont be exactly two ether because when you send money to 
-        // an account you have to pay a certain amount of gas
-        // for that transaction
-        const difference = finalBalance - initialBalance; 
-        assert(difference > web3.utils.toWei('1.8','ether'));
-        
-        // assert that ballot itself has a balance of zero
-        const ballotBalance = await web3.eth.getBalance(ballot.options.address);
-        assert(ballotBalance == 0); 
-
-        // assert that the players array is indeed empty 
-        const players = await ballot.methods.getPlayers().call();
-        assert(players.length == 0); 
-
-    });
+//    it('allows mutiple account to enter', async() => {
+//        
+//        // 1 account enters
+//        await ballot.methods.enter().send({
+//            from: accounts[0], 
+//            value: web3.utils.toWei('0.02', 'ether') 
+//        }); 
+//        // 2 accounts enter 
+//        await ballot.methods.enter().send({
+//            from: accounts[1], 
+//            value: web3.utils.toWei('0.02', 'ether') 
+//        }); 
+//        // 3 accounts enter 
+//        await ballot.methods.enter().send({
+//            from: accounts[2], 
+//            value: web3.utils.toWei('0.02', 'ether') 
+//        }); 
+//
+//
+//        const players = await ballot.methods.getPlayers().call({
+//            from: accounts[0]
+//        });
+//        
+//        assert.equal(accounts[0], players[0]);  // is, should be 
+//        assert.equal(accounts[1], players[1]);  // is, should be 
+//        assert.equal(accounts[2], players[2]);  // is, should be 
+//        assert.equal(3, players.length); 
+//    });
+//
+//    // make sure person can enter if they send less than more ether
+//    it('requires a minimum amt. of ether to enter', async() => {
+//        try{
+//            await ballot.methods.enter().send({
+//                from: accounts[0], 
+//                value: 200 // Wei
+//            });
+//            assert(false); // test fails here  
+//        } catch(err){
+//           assert(err); // check for truthfulness, we should get here cause no ether :( 
+//           
+//        }
+//
+//    });
+//    
+//    // if some person other than the manager attemps to 
+//    // pick a winner an error should be thrown
+//    it('only manager can call pickWinner', async() => {
+//        try{
+//            await ballot.methods.pickWinner().send({
+//                from: accounts[1]
+//            });
+//
+//            assert(false); //automatically fails the test no matter what
+//        } catch(err) {
+//            assert(err); // error should be thrown since ACCT.1 is not the manager (acct 0 is)  
+//        }
+//    });
+//
+//    // end to end test
+//    // winner gets money, players array gets reset
+//    it('sends money to the winner and resets the players array', async() => {
+//        await ballot.methods.enter().send({
+//            from: accounts[0],
+//            value: web3.utils.toWei('2', 'ether')
+//        });
+//
+//        // only enter accounts[0] into ballot so that it def wins
+//        // see if accounts[0] balance before and after is different
+//        // should be more after calling pickWinner
+//        const initialBalance = await web3.eth.getBalance(accounts[0]); // units of wei
+//        
+//        // spent gas here 
+//        await ballot.methods.pickWinner().send({from: accounts[0]});
+//        const finalBalance = await web3.eth.getBalance(accounts[0]);
+//
+//        // wont be exactly two ether because when you send money to 
+//        // an account you have to pay a certain amount of gas
+//        // for that transaction
+//        const difference = finalBalance - initialBalance; 
+//        assert(difference > web3.utils.toWei('1.8','ether'));
+//        
+//        // assert that ballot itself has a balance of zero
+//        const ballotBalance = await web3.eth.getBalance(ballot.options.address);
+//        assert(ballotBalance == 0); 
+//
+//        // assert that the players array is indeed empty 
+//        const players = await ballot.methods.getPlayers().call();
+//        assert(players.length == 0); 
+//
+//    });
 
 });
 
