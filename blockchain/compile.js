3d790eeede653c9de5f4db1cda507925a05618fa
Jennifer de Souza <jenniferdesouza88@gmail.com>
2019-04-27 18:13:23 -0400

    Initial commit of ballot project

---
+++ blockchain/compile.js
@@ -0,0 +1,16 @@
+const path = require('path'); 
+const fs = require('fs'); 
+const solc = require('solc'); 
+
+const ballotPath = path.resolve(__dirname, 'contracts', 'Ballot.sol');
+const source = fs.readFileSync(ballotPath, 'utf8');
+
+//console.log(solc.compile(source, 1)); 
+
+// two properties
+// interface: javascript API
+// bytecode: raw compiled contract 
+module.exports = solc.compile(source, 1).contracts[':Ballot']; 
+
+
+
