1c2f94bb2f2103826cd909657d0700237a710a76
Jennifer de Souza <jenniferdesouza88@gmail.com>
2019-05-01 03:34:34 -0400

    Uploading final presentation video as well as my most recently deploted ballot.js interface for the front end app

--- blockchain-react/src/ballot.js
+++ blockchain-react/src/ballot.js
@@ -2,7 +2,13 @@ import web3 from './web3';
 
 //const address = '0x3A7fFdb5eCD5dd9e1b1ACDc146018F60d826309A'; 
 //const address = '0x8c7d4023d24E964e5163a531499c87eD4ec6Fc2D';
-const address = '0x26F662dc00A250C10Cb57797E6c6899c9B67F66B'; 
+//const address = '0x26F662dc00A250C10Cb57797E6c6899c9B67F66B'; 
+//
+//const address = '0x0c0A3B67F430AefC1a741d4dF400445D02AB7658';
+//
+//const address = '0xeB30e1F5C364F66350FF0d5a17c6EB40c779040b';
+//
+const address = '0x1a555555EBF2c9CC5f661A79f29b5B4c4AfdA465' ;
 const abi = [
     {
         "constant":true,
